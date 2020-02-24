ef4d0ba4c7bb148d77ad1c340143eeb0bf5a1526
tamiwo <tamiwo.otutama@gmail.com>
2020-02-24 19:36:19 +0900

    update:ボタン押して回転

---
+++ unity1week14_reverse/Assets/Scripts/RotateManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform basePanel = default;
+
+    public void Reverse ()
+    {
+        basePanel.Rotate(new Vector3(0f,0f,180f));
+    }
+}
