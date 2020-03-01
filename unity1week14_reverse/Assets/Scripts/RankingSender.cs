43af0d14414776c2e1fdf02497332be6a9ded4ff
tamiwo <tamiwo.otutama@gmail.com>
2020-03-01 16:37:34 +0900

    update:ランキング読み込み

---
+++ unity1week14_reverse/Assets/Scripts/RankingSender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using otutama;
+
+public class RankingSender : MonoBehaviour
+{
+    [SerializeField]
+    private SaveDataInt score = default;
+
+    public void Start()
+    {
+        score.Load();
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking (score.data);
+    }
+}
